3bfb5ce3707d793340b9cf30020ce40223edd08d
tausifabid12 <tasifabid12@gmail.com>
2023-04-05 19:17:17 +0600

    contact page created

--- src/pages/contactUs.tsx
+++ src/pages/contactUs.tsx
@@ -5,12 +5,14 @@ import {
   FormControl,
   FormHelperText,
   Grid,
-  Input,
-  InputLabel,
-  makeStyles,
+  Box,
   TextField,
   Typography,
+  Paper,
 } from '@mui/material';
+import MainLayout from '@/Layouts/MainLayout';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import SendIcon from '@mui/icons-material/Send';
 // import { Send } from '@material-ui/icons';
 
 const ContactUs = () => {
@@ -21,65 +23,205 @@ const ContactUs = () => {
   const [emailError, setEmailError] = useState(false);
   const [messageError, setMessageError] = useState(false);
   return (
-    <Container maxWidth="md">
-      <Typography variant="h4" align="center" gutterBottom>
-        Contact Us
-      </Typography>
+    <MainLayout>
+      <Paper elevation={0} sx={{ mt: { xs: '70px', sm: '110px' } }}>
+        <Box
+          sx={{
+            background: `linear-gradient( rgba(0,0,0, .5), rgba(0,0,0, .5)),  url('/assets/hero.jpg')`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            height: '250px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography variant="h3" sx={{ color: 'white', fontWeight: '600' }}>
+            Contact Us
+          </Typography>
+        </Box>
+        {/* <Typography variant="h4" align="center" gutterBottom>
+          Contact Us
+        </Typography> */}
+        <Grid container spacing={2} sx={{ py: 15, px: { xs: 2, sm: 10 } }}>
+          <Grid item xs={12} sm={6} sx={{ mb: { xs: 7, lg: 0 } }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Typography
+                sx={{
+                  color: 'primary.main',
+                  bgcolor: 'primary.main',
+                  borderColor: 'primary.main',
+                  width: '80px',
+                  height: '80px',
+                  borderRadius: '50%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  mr: 2,
+                }}
+              >
+                <LocationOnIcon sx={{ color: 'white' }} />
+              </Typography>
+              <Box>
+                <Typography
+                  variant="h5"
+                  sx={{ fontWeight: '600', color: 'primary.main' }}
+                >
+                  Address
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: '600', color: 'text_color.main' }}
+                >
+                  Gaibandha-5700, Rangpur, Bangladesh
+                </Typography>
+              </Box>
+            </Box>
+            <Box sx={{ display: 'flex', alignItems: 'center', mt: 3 }}>
+              <Typography
+                sx={{
+                  color: 'primary.main',
+                  bgcolor: 'primary.main',
+                  borderColor: 'primary.main',
+                  width: '80px',
+                  height: '80px',
+                  borderRadius: '50%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  mr: 2,
+                }}
+              >
+                <LocationOnIcon sx={{ color: 'white' }} />
+              </Typography>
+              <Box>
+                <Typography
+                  variant="h5"
+                  sx={{ fontWeight: '600', color: 'primary.main' }}
+                >
+                  Address
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: '600', color: 'text_color.main' }}
+                >
+                  Gaibandha-5700, Rangpur, Bangladesh
+                </Typography>
+              </Box>
+            </Box>
+            <Box sx={{ display: 'flex', alignItems: 'center', mt: 3 }}>
+              <Typography
+                sx={{
+                  color: 'primary.main',
+                  bgcolor: 'primary.main',
+                  borderColor: 'primary.main',
+                  width: '80px',
+                  height: '80px',
+                  borderRadius: '50%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  mr: 2,
+                }}
+              >
+                <LocationOnIcon sx={{ color: 'white' }} />
+              </Typography>
+              <Box>
+                <Typography
+                  variant="h5"
+                  sx={{ fontWeight: '600', color: 'primary.main' }}
+                >
+                  Address
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: '600', color: 'text_color.main' }}
+                >
+                  Gaibandha-5700, Rangpur, Bangladesh
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
 
-      <form>
-        <Grid container spacing={2}>
+          {/* from */}
           <Grid item xs={12} sm={6}>
-            <TextField
-              id="name"
-              label="Name"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-              error={nameError}
-              helperText={nameError && 'Please enter your name'}
-              fullWidth
-              required
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="email"
-              label="Email"
-              type="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              error={emailError}
-              helperText={emailError && 'Please enter a valid email address'}
-              fullWidth
-              required
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="message"
-              label="Message"
-              multiline
-              rows={4}
-              value={message}
-              onChange={(event) => setMessage(event.target.value)}
-              error={messageError}
-              helperText={messageError && 'Please enter your message'}
-              fullWidth
-              required
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              variant="contained"
-              color="primary"
-              //   startIcon={<Send />}
-              type="submit"
-            >
-              Send
-            </Button>
+            <form>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="name"
+                    label="Name"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                    error={nameError}
+                    helperText={nameError && 'Please enter your name'}
+                    fullWidth
+                    required
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="email"
+                    label="Email"
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    error={emailError}
+                    helperText={
+                      emailError && 'Please enter a valid email address'
+                    }
+                    fullWidth
+                    required
+                  />
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                  <TextField
+                    id="email"
+                    label="Phone"
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    error={emailError}
+                    helperText={
+                      emailError && 'Please enter a valid email address'
+                    }
+                    fullWidth
+                    required
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="message"
+                    label="Message"
+                    multiline
+                    rows={4}
+                    value={message}
+                    onChange={(event) => setMessage(event.target.value)}
+                    error={messageError}
+                    helperText={messageError && 'Please enter your message'}
+                    fullWidth
+                    required
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                    startIcon={<SendIcon />}
+                    type="submit"
+                    sx={{ px: 5, py: 2 }}
+                  >
+                    Send
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
           </Grid>
         </Grid>
-      </form>
-    </Container>
+      </Paper>
+    </MainLayout>
   );
 };
 
